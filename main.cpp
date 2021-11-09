09f03dad8ab3de79080f293266fcf71221395e20
liangfengjnu <1163920098@qq.com>
2021-11-09 21:06:04 +0800

    11091

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+
+#include "locker.h"
+#include "threadpool.h"
+#inculde "httpconn.h"
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
+
+extern int addfd(int epollfd, int fd, bool one_shot);
+extern int removefd(int epollfd, int fd);
+
+
+
+int main(int argc, char* argv[]){
+	
+	return 0;
+}
